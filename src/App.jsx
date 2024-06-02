cc72500fef33149ad2ddfdf01fa6e979ee2f0deb
rizzyad <rizzyadramadhan@gmail.com>
2024-06-02 18:18:03 +0700

    add delete function and fetch data from array

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import { items, start } from "./components/Navbar";
-import { columns, header, footer } from "./components/Content";
-import { useState } from "react";
+import { columns, header, Footer, tableData, actionBodyTemplate } from "./components/Content";
+import { useState, useEffect } from "react";
 import { Menubar } from "primereact/menubar";
 import { DataTable } from "primereact/datatable";
 import { Column } from "primereact/column";
 
 function App() {
-  const [transaction, setTransaction] = useState(0);
+  const [transaction, setTransaction] = useState([]);
+
+  useEffect(() => {
+    setTransaction(tableData);
+  }, []);
+
+  const handleDelete = (rowData) => {
+    setTransaction(transaction.filter((item) => item.id !== rowData.id));
+  };
 
   return (
     <>
@@ -24,18 +32,17 @@ function App() {
             <DataTable
               value={transaction}
               header={header}
-              footer={footer}
+              footer={<Footer transaction={transaction}/>}
               tableStyle={{ minWidth: "60rem" }}
             >
-              {columns.map((col, i) => (
+              {columns.map((col) => (
                 <Column key={col.field} field={col.field} header={col.header} />
               ))}
+              <Column body={(rowData) => actionBodyTemplate(rowData, { handleDelete })} header="Action" />
             </DataTable>
           </div>
         </div>
       </div>
-
-
     </>
   );
 }
--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,5 +1,62 @@
 import { Button } from "primereact/button";
 
+export const columns = [
+  { field: "id", header: "ID" },
+  { field: "car", header: "Car" },
+  { field: "customer", header: "Customer" },
+  { field: "rentalDate", header: "Rental Date" },
+  { field: "returnDate", header: "Return Date" },
+  { field: "rentalFees", header: "Rental Fees" },
+];
+
+export const tableData = [
+  {
+    id: 1,
+    car: "Avanza",
+    customer: "Adi",
+    rentalDate: "20/05/2024",
+    returnDate: "23/05/2024",
+    rentalFees: "100000",
+  },
+  {
+    id: 2,
+    car: "Lambo",
+    customer: "Eka",
+    rentalDate: "25/05/2024",
+    returnDate: "30/05/2024",
+    rentalFees: "200000",
+  },
+  {
+    id: 3,
+    car: "Ferrari",
+    customer: "Dian",
+    rentalDate: "22/05/2024",
+    returnDate: "28/05/2024",
+    rentalFees: "700000",
+  },
+];
+
+export const actionBodyTemplate = (rowData, { handleDelete }) => {
+  return (
+    <>
+      <Button
+        icon="pi pi-pen-to-square"
+        severity="warning"
+        rounded
+        raised
+        style={{ marginRight: "5px" }}
+      />
+      <Button
+        icon="pi pi-trash"
+        severity="danger"
+        rounded
+        raised
+        onClick={() => handleDelete(rowData)}
+      />
+    </>
+  );
+};
+
 export const header = (
   <div className="flex flex-wrap align-items-center justify-content-between gap-2">
     <span className="text-xl text-900 font-bold">Transaction Data</span>
@@ -7,16 +64,6 @@ export const header = (
   </div>
 );
 
-export const footer = `In total there are 0 transaction.`;
-
-export const columns = [
-  { field: "no", header: "No" },
-  { field: "car", header: "Car" },
-  { field: "customer", header: "Customer" },
-  { field: "rentalDate", header: "Rental Date" },
-  { field: "returnDate", header: "Return Date" },
-  { field: "rentalFees", header: "Rental Fees" },
-  { field: "action", header: "Action" },
-];
-
-
+export const Footer = ({ transaction }) => {
+  return `In total there are ${transaction ? transaction.length : 0} transaction.`;
+};

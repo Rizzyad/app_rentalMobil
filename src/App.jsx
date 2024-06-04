e6c531ffe63b987cb966db8ebd375d8775b4c58e
rizzyad <rizzyadramadhan@gmail.com>
2024-06-04 09:51:41 +0700

    refactored and all the function is works, next target is car CRUD table

--- src/App.jsx
+++ src/App.jsx
@@ -13,10 +13,10 @@ import { useState, useEffect, useRef } from "react";
 import { Menubar } from "primereact/menubar";
 import { DataTable } from "primereact/datatable";
 import { Column } from "primereact/column";
-import { Button, TextInput, Modal, NumberInput, Select } from "@mantine/core";
-import { DateInput } from "@mantine/dates";
+import { Modal } from "@mantine/core";
 import { useDisclosure } from "@mantine/hooks";
 import { calculateDays, formatDate } from "./functions/Functions";
+import { FormModalInsert, FormModalEdit } from "./components/FormModal";
 
 function App() {
   const [transaction, setTransaction] = useState([]);
@@ -333,121 +333,3 @@ function App() {
 }
 
 export default App;
-
-const FormModalInsert = ({
-  carsData,
-  handleSubmit,
-  newTransaction,
-  close,
-  handleInputChange,
-}) => {
-  return (
-    <form onSubmit={handleSubmit}>
-      <Select
-        data-autofocus
-        label="Car"
-        placeholder="Select the car"
-        data={carsData.map((car) => ` ${car.name}  : ${car.pricePerDay} /day`)}
-        value={newTransaction.car || ""}
-        onChange={(value) => handleInputChange("car", value)}
-        required
-      />
-      <TextInput
-        label="Name"
-        placeholder="input your name"
-        value={newTransaction.customer || ""}
-        onChange={(event) => handleInputChange("customer", event.target.value)}
-        required
-      />
-      <DateInput
-        label="Rental Date"
-        placeholder="input rental date"
-        value={newTransaction.rentalDate || ""}
-        onChange={(value) => handleInputChange("rentalDate", value)}
-        required
-      />
-      <DateInput
-        label="Finish Date"
-        placeholder="input finish date"
-        value={newTransaction.returnDate || ""}
-        onChange={(value) => handleInputChange("returnDate", value)}
-        required
-      />
-      <NumberInput
-        label="Rental Fees"
-        placeholder="total rental fees"
-        value={newTransaction.rentalFees || ""}
-        onChange={(value) => handleInputChange("rentalFees", value)}
-        readOnly
-        required
-      />
-      <div className="flex justify-content-end">
-        <Button variant="filled" color="red" mr="sm" mt="md" onClick={close}>
-          Cancel
-        </Button>
-        <Button variant="filled" mt="md" type="submit">
-          Submit
-        </Button>
-      </div>
-    </form>
-  );
-};
-
-const FormModalEdit = ({
-  handleEditSubmit,
-  carsData,
-  handleInputEdit,
-  selectedTransaction,
-  close2,
-}) => {
-  return (
-    <form onSubmit={handleEditSubmit}>
-      <Select
-        data-autofocus
-        label="Car"
-        placeholder="Select the car"
-        data={carsData.map((car) => ` ${car.name}  : ${car.pricePerDay} /day`)}
-        value={selectedTransaction?.car || ""}
-        onChange={(value) => handleInputEdit("car", value)}
-        required
-      />
-      <TextInput
-        label="Name"
-        placeholder="input your name"
-        value={selectedTransaction?.customer || ""}
-        onChange={(event) => handleInputEdit("customer", event.target.value)}
-        required
-      />
-      <DateInput
-        label="Rental Date"
-        placeholder="input rental date"
-        value={selectedTransaction?.rentalDate || ""}
-        onChange={(value) => handleInputEdit("rentalDate", value)}
-        required
-      />
-      <DateInput
-        label="Finish Date"
-        placeholder="input finish date"
-        value={selectedTransaction?.returnDate || ""}
-        onChange={(value) => handleInputEdit("returnDate", value)}
-        required
-      />
-      <NumberInput
-        label="Rental Fees"
-        placeholder="total rental fees"
-        value={selectedTransaction?.rentalFees || ""}
-        onChange={(value) => handleInputEdit("rentalFees", value)}
-        readOnly
-        required
-      />
-      <div className="flex justify-content-end">
-        <Button variant="filled" color="red" mr="sm" mt="md" onClick={close2}>
-          Cancel
-        </Button>
-        <Button variant="filled" mt="md" type="submit">
-          Submit
-        </Button>
-      </div>
-    </form>
-  );
-};
---
+++ src/components/FormModal.jsx
@@ -0,0 +1,122 @@
+import { Button, TextInput, NumberInput, Select } from "@mantine/core";
+import { DateInput } from "@mantine/dates";
+
+export const FormModalInsert = ({
+  carsData,
+  handleSubmit,
+  newTransaction,
+  close,
+  handleInputChange,
+}) => {
+  return (
+    <form onSubmit={handleSubmit}>
+      <Select
+        data-autofocus
+        label="Car"
+        placeholder="Select the car"
+        data={carsData.map((car) => ` ${car.name}  : ${car.pricePerDay} /day`)}
+        value={newTransaction.car || ""}
+        onChange={(value) => handleInputChange("car", value)}
+        required
+      />
+      <TextInput
+        label="Name"
+        placeholder="input your name"
+        value={newTransaction.customer || ""}
+        onChange={(event) => handleInputChange("customer", event.target.value)}
+        required
+      />
+      <DateInput
+        label="Rental Date"
+        placeholder="input rental date"
+        value={newTransaction.rentalDate || ""}
+        onChange={(value) => handleInputChange("rentalDate", value)}
+        required
+      />
+      <DateInput
+        label="Finish Date"
+        placeholder="input finish date"
+        value={newTransaction.returnDate || ""}
+        onChange={(value) => handleInputChange("returnDate", value)}
+        required
+      />
+      <NumberInput
+        label="Rental Fees"
+        placeholder="total rental fees"
+        value={newTransaction.rentalFees || ""}
+        onChange={(value) => handleInputChange("rentalFees", value)}
+        readOnly
+        required
+      />
+      <div className="flex justify-content-end">
+        <Button variant="filled" color="red" mr="sm" mt="md" onClick={close}>
+          Cancel
+        </Button>
+        <Button variant="filled" mt="md" type="submit">
+          Submit
+        </Button>
+      </div>
+    </form>
+  );
+};
+
+
+export const FormModalEdit = ({
+    handleEditSubmit,
+    carsData,
+    handleInputEdit,
+    selectedTransaction,
+    close2,
+  }) => {
+    return (
+      <form onSubmit={handleEditSubmit}>
+        <Select
+          data-autofocus
+          label="Car"
+          placeholder="Select the car"
+          data={carsData.map((car) => ` ${car.name}  : ${car.pricePerDay} /day`)}
+          value={selectedTransaction?.car || ""}
+          onChange={(value) => handleInputEdit("car", value)}
+          required
+        />
+        <TextInput
+          label="Name"
+          placeholder="input your name"
+          value={selectedTransaction?.customer || ""}
+          onChange={(event) => handleInputEdit("customer", event.target.value)}
+          required
+        />
+        <DateInput
+          label="Rental Date"
+          placeholder="input rental date"
+          value={selectedTransaction?.rentalDate || ""}
+          onChange={(value) => handleInputEdit("rentalDate", value)}
+          required
+        />
+        <DateInput
+          label="Finish Date"
+          placeholder="input finish date"
+          value={selectedTransaction?.returnDate || ""}
+          onChange={(value) => handleInputEdit("returnDate", value)}
+          required
+        />
+        <NumberInput
+          label="Rental Fees"
+          placeholder="total rental fees"
+          value={selectedTransaction?.rentalFees || ""}
+          onChange={(value) => handleInputEdit("rentalFees", value)}
+          readOnly
+          required
+        />
+        <div className="flex justify-content-end">
+          <Button variant="filled" color="red" mr="sm" mt="md" onClick={close2}>
+            Cancel
+          </Button>
+          <Button variant="filled" mt="md" type="submit">
+            Submit
+          </Button>
+        </div>
+      </form>
+    );
+  };
+  
